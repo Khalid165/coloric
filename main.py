1a7458bf80480e4d70cb16eee7798d7cc72d280a
Khalid Baowaidhan <Khalidbaowaidhan@gmail.com>
2022-11-11 07:26:41 +0300

    final project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for,flash
+from flask import Flask,render_template,request,redirect,flash
 from flask_bootstrap import Bootstrap
 import os
 from werkzeug.utils import secure_filename
